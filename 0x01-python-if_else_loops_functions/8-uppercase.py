3e9e4b90647237e94407e40b622916ff7e4928b0
CalebAgyei <calebnagyei@gmail.com>
2022-11-29 22:00:13 -0800

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        if (ord(ch) >= 97 and ord(ch) <= 122):
+            c = chr(ord(ch) - 32)
+            print("{}".format(c), end="")
+        else:
+            print("{}".format(ch), end="")
