b062652d1d6be0d82ff9cc9a7fdebed8ef180779
CalebAgyei <calebnagyei@gmail.com>
2022-12-13 14:05:13 -0800

    Print to stdout without using print, eval, open, import sys

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,7 +10,11 @@ if __name__ == "__main__":
         print("{} + {} = {}".format(int(argv[1]), int(argv[3]), add(int(argv[1]), int(argv[3]))))
     elif argv[2] == "-":
         print("{} - {} = {}".format(int(argv[1]), int(argv[3]), sub(int(argv[1]), int(argv[3]))))
-    elif argv[2] == "*":
+    elif argv[2] == '*':
         print("{} * {} = {}".format(int(argv[1]), int(argv[3]), mul(int(argv[1]), int(argv[3]))))
     elif argv[2] == "/":
         print("{} / {} = {}".format(int(argv[1]), int(argv[3]), div(int(argv[1]), int(argv[3]))))
+
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
