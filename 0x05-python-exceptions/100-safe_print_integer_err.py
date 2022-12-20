7a6331b787812d8966d8e15e5fb038d09025ebf9
CalebAgyei <calebnagyei@gmail.com>
2022-12-19 20:40:37 -0800

    Task 7 - print integer and raise error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except ValueError as exc:
+        return False
         print("Exception:", exc.args)
     else:
         return True
