86566fde953c222f81624e8db86b1aea5e67b46c
CalebAgyei <calebnagyei@gmail.com>
2022-12-20 18:07:17 -0800

    Task 2 - Square class with TypeError and ValueError handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,8 +12,8 @@ class Square:
         Args:
             size: The size of the square
         """
-        self.__size = size
-        if type(self.__size) != 'int':
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
