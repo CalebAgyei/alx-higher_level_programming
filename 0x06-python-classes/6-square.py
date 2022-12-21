e3e5bd0e9f98906c6f907b7576b9736a53c38420
CalebAgyei <calebnagyei@gmail.com>
2022-12-20 20:09:55 -0800

    Task 6 - print square with coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,4 +66,4 @@ class Square:
             print("")
         else:
             for i in range(self.size):
-                print("_" * self.position[0] + "#" * self.size)
+                print(" " * self.position[0] + "#" * self.size)
