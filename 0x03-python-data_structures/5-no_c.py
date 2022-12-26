df22b5fefbdf250a4055e273900406504570cbf7
CalebAgyei <calebnagyei@gmail.com>
2022-12-26 15:10:49 -0800

    Task 5 - Remove all characters c and C from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     """Removes all characters 'c' and 'C' from string."""
     new_string = ""
     for i in range(len(my_string)):
-        if (ord(my_string[i]) == 99) or (ord(my_string[i]) == 67):
+        if (my_string[i] == 'c') or (my_string[i] == 'C'):
             new_string
         else:
             new_string += my_string[i]
