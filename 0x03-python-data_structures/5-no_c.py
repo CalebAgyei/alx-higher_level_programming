d874f2e234a8016445467a88a5b43695db2568a8
CalebAgyei <calebnagyei@gmail.com>
2022-12-26 15:09:04 -0800

    Task 5 - Remove all characters c and C from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    """Removes all characters 'c' and 'C' from string."""
     new_string = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'c') or (my_string[i] != 'C'):
+        if (ord(my_string[i]) == 99) or (ord(my_string[i]) == 67):
+            new_string
+        else:
             new_string += my_string[i]
     return new_string
