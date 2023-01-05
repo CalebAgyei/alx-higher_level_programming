9ddede9cbcbc4878a49aaafe2c19389e7f56e099
CalebAgyei <calebnagyei@gmail.com>
2023-01-04 16:53:30 -0800

    Sundry commits

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """Adds 2 integers.
+
+    Args:
+    @a: First integer.
+    @b: Second integer.
+
+    Return:
+        Sum of two integers.
+    """
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+0x07-python-test_driven_development
+Task 0 - Write a function that adds 2 integers
+Task 1 - Write a function that divides all elements of a matrix.
