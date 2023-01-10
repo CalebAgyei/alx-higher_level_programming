6bda9a017eca7d7f5854e3ebfd75e803a34fee68
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 15:44:54 -0800

    Task 1 - Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Creates a function that writes a string.""" 
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file in UTF8.
+
+    Args:
+        @filename: Name of the text file to read.
+        @text: Test to write to file.
+
+    Return:
+        Number of characters written.
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        write_file = f.write(text)
+        return (write_file)
