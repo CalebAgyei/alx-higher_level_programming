a3944593bf16f18b1e1209eb317cde7ea9f28e65
CalebAgyei <calebnagyei@gmail.com>
2023-01-09 20:08:59 -0800

    Task 0 - Function that returns the list of attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines an object attribute and method lookup function."""
+
 
 def lookup(obj):
     """Returns the list of all available attributes and methods of an object.
@@ -7,7 +9,7 @@ def lookup(obj):
         @obj: object whose attributes and methods need to be listed
     """
 
-    return list(dir(obj))
+    return (dir(obj))
 
 if __name__ == "__main__":
     pass
