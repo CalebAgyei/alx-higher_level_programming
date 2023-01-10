9654161ffc6511e57184a11eeea68efbe2bf73f9
CalebAgyei <calebnagyei@gmail.com>
2023-01-09 22:34:20 -0800

    Task 8 - rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validates integer."""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
@@ -30,8 +30,7 @@ class Rectangle(BaseGeometry):
             @width: Width of rectangle.
             @height: Height of rectangle.
         """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
