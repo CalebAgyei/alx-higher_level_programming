fa17df5e72e6bd5c87c4c932965223881ae3421f
CalebAgyei <calebnagyei@gmail.com>
2023-01-09 20:48:34 -0800

    Task 2 - Checks type of obj

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,9 +3,13 @@
 
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class."""
+    """Returns True if obj is exactly an instance of a_class.
 
-    if not isinstance(obj, a_class):
-        return False
+    Args:
+        @obj: The object to check.
+        @a_class: The class to match to the type of obj to."""
 
-    return (True)
+    if type(obj) == a_class:
+        return True
+
+    return False
