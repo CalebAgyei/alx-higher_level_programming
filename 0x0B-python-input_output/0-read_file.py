d6cb3dc12df0df02fd7386b900724d2c5d1af3e3
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 15:17:05 -0800

    Task 0 - function that reads files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creates a function that reads a text file (UTF8) and prints to stdout."""
+
+
+def read_file(filename=""):
+    """Read text files in UTF8.
+
+    Args:
+        @filename: Name of the text file to read.
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
