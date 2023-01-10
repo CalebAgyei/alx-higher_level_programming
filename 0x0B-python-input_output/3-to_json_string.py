1f0a29695926c883e9764f15294d0cacf20bcb69
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 15:59:47 -0800

    Task 3 - To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+
+"""Defines a function to_json_string."""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)."""
+
+    return (json.dumps(my_obj))
