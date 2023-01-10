944d4af84e8c186cf7497c85f5a7431ee877705c
CalebAgyei <calebnagyei@gmail.com>
2023-01-09 21:33:52 -0800

    Task 6 - Class with Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Creates an class BaseGeometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
