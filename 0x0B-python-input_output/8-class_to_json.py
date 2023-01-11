3d5d01f9c92ae1be883e368a34a8a1991c43df71
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 20:10:53 -0800

    Task 8 - Serilazing python hierarchies with JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a function class_to_join."""
+
+
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description for JSON.
+    Args:
+        @obj: An instance of a Class.
+    """
+
+    try:
+        return json.dumps(obj)
+
+    except Exception:
+        pass
