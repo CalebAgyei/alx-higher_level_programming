86774db3d9a32903b52663f93617f5484f3a5a9f
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 21:47:45 -0800

    Task 10 - JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student.
+
+
+        Args:
+            @first_name: Student's first name
+            @last_name: Student's last name
+            @age: Age of student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            @attrs: Attributes to be used"""
+
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
