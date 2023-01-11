9c6f5c7004415c0d4f82e2e19eefaf99c3386fbc
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 19:29:58 -0800

    Task 7 - Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a Python list, and save them to a file."""
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    from sys import argv
+
+    filename = "add_item.json"
+
+    obj = []
+
+    if len(argv) == 1:
+        save_to_json_file(obj, filename)
+
+    elif len(argv) >= 2:
+        for i in range(1, len(argv)):
+            obj.append(str(argv[i]))
+
+            save_to_json_file(obj, filename)
