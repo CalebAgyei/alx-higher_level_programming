a17896507f3fa11328b2558d754a4c1cf8a0a492
CalebAgyei <calebnagyei@gmail.com>
2023-01-10 20:57:07 -0800

    Task 9 - Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a class student."""
+
+
+import json
+
+
+class Student:
+    """Represents a student."""
+    
+    def __init__(self, first_name, last_name, age):
+        """Initializes student.
+
+        Args:
+            @first_name: Student's first name
+            @last_name: Student's last name
+            @age: Age of student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+
+        return self.__dict__
