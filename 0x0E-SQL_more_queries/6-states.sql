7a012ced65a099f9e667e6f7559fdb2cfc3fed14
CalebAgyei <calebnagyei@gmail.com>
2023-03-21 05:54:13 -0700

    Set primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,16 @@
+-- Create a database and a table
+-- `id` INT unique, auto generated, can't be null and is primary key
+
+-- Create a database 'hbtn_0d_usa'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switch to new database
+USE hbtn_0d_usa;
+
+-- Create a table 'states' in database 'hbtn_0d_usa'
+CREATE TABLE states (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  -- UNIQUE (id) // Primary key is a UNIQUE key
+);
