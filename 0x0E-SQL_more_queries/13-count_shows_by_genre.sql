72b6034f225f376979dba28998e7214492a5d1a7
CalebAgyei <calebnagyei@gmail.com>
2023-03-21 09:24:46 -0700

    RIGHT JOIN, GROUP BY, Aliases

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- List all genres from hbtn_0d_tvshows and display the number of shows linked to each
--- Using COUNT and GROUP BY
+-- Using RIGHT JOIN, COUNT and GROUP BY
 
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) as number_of_shows
   FROM tv_genres
-  JOIN tv_show_genres
+  RIGHT JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
-  GROUP BY tv_genres.name
-  ORDER BY COUNT(tv_show_genres.genre_id) DESC;
+  GROUP BY genre
+  ORDER BY number_of_shows DESC;
